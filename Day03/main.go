48e38e08c81f53c4975c3a32303e060d3447f16d
Jenn Hernes <jenn_hernes@hotmail.com>
2024-12-03 22:36:03 +0200

    use submatch to improve day 03

--- Day03/main.go
+++ Day03/main.go
@@ -16,85 +16,56 @@ func main() {
 		return
 	}
 
-	mulRegex, err := regexp.Compile("mul\\(\\d{1,3},\\d{1,3}\\)")
+	fullRegex, err := regexp.Compile("(mul\\((\\d{1,3}),(\\d{1,3})\\))")
 	if err != nil {
-		fmt.Printf("Could not compile regex main: %v\n", err)
+		fmt.Printf("Could not compile regex full: %v\n", err)
 		return
 	}
-	numRegex, err := regexp.Compile("\\d{1,3}")
-	if err != nil {
-		fmt.Printf("Could not compile regex nums: %v\n", err)
-		return
-	}
-
-	matches := mulRegex.FindAllString(string(contents), -1)
 
 	total := 0
-	for _, match := range matches {
-		prod := 1
-		//fmt.Println(match)
-		sa := numRegex.FindAllString(match, 2)
-		for _, s := range sa {
-			n, err := strconv.Atoi(s)
-			if err != nil {
-				fmt.Printf("Tried to convert %v to int: %v\n", s, err)
-				return
-			}
-			prod *= n
+	for _, match := range fullRegex.FindAllStringSubmatch(string(contents), -1) {
+		//fmt.Printf("%v\n", match)
+		x, err := strconv.Atoi(match[2])
+		if err != nil {
+			fmt.Printf("Could not convert %v: %v\n", match[2], err)
+			return
 		}
-		total += prod
+		y, err := strconv.Atoi(match[3])
+		if err != nil {
+			fmt.Printf("Could not convert %v: %v\n", match[3], err)
+			return
+		}
+		total += x * y
 	}
 
 	fmt.Printf("Part 1: %v\n", total)
 
-	doRegex, err := regexp.Compile("do\\(\\)")
+	fullRegex, err = regexp.Compile("(mul\\((\\d{1,3}),(\\d{1,3})\\))|(do\\(\\))|(don't\\(\\))")
 	if err != nil {
-		fmt.Printf("Could not compile regex do(): %v\n", err)
+		fmt.Printf("Could not compile regex full: %v\n", err)
 		return
 	}
-
-	dontRegex, err := regexp.Compile("don't\\(\\)")
-	if err != nil {
-		fmt.Printf("Could not compile regex do(): %v\n", err)
-		return
-	}
-
-	dos := doRegex.FindAllStringIndex(string(contents), -1)
-	donts := dontRegex.FindAllStringIndex(string(contents), -1)
-	matchIndices := mulRegex.FindAllStringIndex(string(contents), -1)
-
+	enabled := true
 	total = 0
-	for i, match := range matches {
-		doIndex := 0
-		dontIndex := -1
-		for _, a := range dos {
-			//fmt.Printf("Index for %v is %v\n", match, matchIndices[i])
-			if a[0] > matchIndices[i][0] {
-				break
-			}
-			doIndex = a[0]
-		}
-		for _, a := range donts {
-			if a[0] > matchIndices[i][0] {
-				break
-			}
-			dontIndex = a[0]
-		}
-		if dontIndex > doIndex {
-			continue
-		}
-		prod := 1
-		//fmt.Println(match)
-		sa := numRegex.FindAllString(match, 2)
-		for _, s := range sa {
-			n, err := strconv.Atoi(s)
+	for _, match := range fullRegex.FindAllStringSubmatch(string(contents), -1) {
+		//fmt.Printf("%v\n", match)
+		if match[0] == "don't()" {
+			enabled = false
+		} else if match[0] == "do()" {
+			enabled = true
+		} else if enabled {
+			x, err := strconv.Atoi(match[2])
 			if err != nil {
-				fmt.Printf("Tried to convert %v to int: %v\n", s, err)
+				fmt.Printf("Could not convert %v: %v\n", match[2], err)
 				return
 			}
-			prod *= n
+			y, err := strconv.Atoi(match[3])
+			if err != nil {
+				fmt.Printf("Could not convert %v: %v\n", match[3], err)
+				return
+			}
+			total += x * y
 		}
-		total += prod
 	}
 
 	fmt.Printf("Part 2: %v\n", total)
